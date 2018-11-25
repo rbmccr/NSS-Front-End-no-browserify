03685cd3e4fdb5b58cc00ac9869eb1d36ef7f977
Brendan McCray <rbmccr@gmail.com>
2018-11-25 00:47:04 -0600

    level method for gameplay layout functioning with buttons. Need to evaluate image responsiveness in containers

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -4,39 +4,56 @@ const gameplay = {
   loadGameplay() {
     webpage.innerHTML = null;
     const activeUser = sessionStorage.getItem("activeUserId");
-    const xButton = elBuilder("button", { "class": "delete" });
-    xButton.addEventListener("click", closeBox, event); // button will display: none on parent container
-    const headerInfo = elBuilder("div", { "class": "notification is-info" }, "Create and save shots - then save the game record.", xButton);
-    webpage.appendChild(headerInfo);
-    this.buildTiles();
+    // const xButton = elBuilder("button", { "class": "delete" });
+    // xButton.addEventListener("click", closeBox, event); // button will display: none on parent container
+    // const headerInfo = elBuilder("div", { "class": "notification is-info" }, "Create and save shots - then save the game record.", xButton);
+    // webpage.appendChild(headerInfo);
+    this.buildContainers();
   },
 
-  buildTiles() {
+  buildContainers() {
 
-    // see commented HTML at bottom of this file to understand format
-    const p3child1 = elBuilder("div", { "id": "gameDetails", "class": "tile is-child box" }, "#4")
-    const group1_sub2 = elBuilder("div", { "class": "tile is-parent" }, null, p3child1);
+    const leftItem1 = elBuilder("div", { "id": "shotControls", "class": "level-item buttons" });
+    const levelLeft1 = elBuilder("div", { "class": "level-left" }, null, leftItem1);
+    const rightItem1 = elBuilder("div", { "id": "", "class": "level-item" }, "Level right 1");
+    const levelRight1 = elBuilder("div", { "id": "shotControls", "class": "level-right" }, null, rightItem1);
+    const levelContainer1 = elBuilder("div", { "class": "level container box" }, null, levelLeft1, levelRight1);
+    webpage.appendChild(levelContainer1);
 
-    const p2child1 = elBuilder("div", { "id": "shotDetails", "class": "tile is-child box" })
-    const p2child2 = elBuilder("div", { "id": "field", "class": "tile is-child box has-text-centered" })
-    const p2child3 = elBuilder("div", { "id": "goal", "class": "tile is-child box" }, "#3.2")
-    const parent2 = elBuilder("div", { "class": "tile is-parent is-vertical" }, null, p2child1, p2child2, p2child3);
+    // const levelLeft2 = elBuilder("div", {"id":"", "class":"level-left"});
+    // const levelRight2 = elBuilder("div", {"id":"", "class":"level-right"});
+    // const levelContainer2 = elBuilder("div", {"class":"level container box"}, null, levelLeft2, levelRight2);
+    // webpage.appendChild(levelContainer2);
 
-    const p1child1 = elBuilder("div", { "id": "shotNewSave", "class": "tile is-child box has-text-centered" })
-    const p1child2 = elBuilder("div", { "id": "shotButtons", "class": "tile is-child box has-text-centered" })
-    const parent1 = elBuilder("div", { "class": "tile is-parent is-vertical is-2" }, null, p1child1, p1child2);
+    // const levelLeft3 = elBuilder("div", {"id":"", "class":"level-left"});
+    // const levelRight3 = elBuilder("div", {"id":"", "class":"level-right"});
+    // const levelContainer3 = elBuilder("div", {"class":"level container box"}, null, levelLeft3, levelRight3);
+    // webpage.appendChild(levelContainer3);
 
-    const group1_sub = elBuilder("div", { "class": "tile" }, null, parent1, parent2);
-    const group1 = elBuilder("div", { "class": "tile is-vertical" }, null, group1_sub, group1_sub2);
+    // // see commented HTML at bottom of this file to understand format
+    // const p3child1 = elBuilder("div", { "id": "gameDetails", "class": "tile is-child box" }, "#4")
+    // const group1_sub2 = elBuilder("div", { "class": "tile is-parent" }, null, p3child1);
 
-    const ancestor = elBuilder("div", { "class": "tile is-ancestor" }, null, group1);
+    // const p2child1 = elBuilder("div", { "id": "shotDetails", "class": "tile is-child box" })
+    // const p2child2 = elBuilder("div", { "id": "field", "class": "tile is-child box has-text-centered" })
+    // const p2child3 = elBuilder("div", { "id": "goal", "class": "tile is-child box" }, "#3.2")
+    // const parent2 = elBuilder("div", { "class": "tile is-parent is-vertical" }, null, p2child1, p2child2, p2child3);
 
-    webpage.appendChild(ancestor);
+    // const p1child1 = elBuilder("div", { "id": "shotNewSave", "class": "tile is-child box has-text-centered" })
+    // const p1child2 = elBuilder("div", { "id": "shotButtons", "class": "tile is-child box has-text-centered" })
+    // const parent1 = elBuilder("div", { "class": "tile is-parent is-vertical is-2" }, null, p1child1, p1child2);
+
+    // const group1_sub = elBuilder("div", { "class": "tile" }, null, parent1, parent2);
+    // const group1 = elBuilder("div", { "class": "tile is-vertical" }, null, group1_sub, group1_sub2);
+
+    // const ancestor = elBuilder("div", { "class": "tile is-ancestor" }, null, group1);
+
+    // webpage.appendChild(ancestor);
     this.addContent();
   },
 
   addContent() {
-    const shotBtns1 = document.getElementById("shotNewSave");
+    const shotBtns1 = document.getElementById("shotControls");
     const shotBtns2 = document.getElementById("shotButtons");
     const shotDetails = document.getElementById("shotDetails");
     const field = document.getElementById("field");
@@ -44,30 +61,33 @@ const gameplay = {
     const gameDetails = document.getElementById("gameDetails");
 
     // new shot and save shot buttons
-    const newShot = elBuilder("button", { "id": "newShot", "class": "button is-success", "style": "margin:2.5px" }, "New Shot")
-    const saveShot = elBuilder("button", { "id": "saveShot", "class": "button is-success", "style": "margin:2.5px" }, "Save Shot")
+    const newShot = elBuilder("button", { "id": "newShot", "class": "button is-success" }, "New Shot");
+    const saveShot = elBuilder("button", { "id": "saveShot", "class": "button is-success" }, "Save Shot");
+    const editGame = elBuilder("button", { "id": "editGame", "class": "button is-danger" }, "Edit Previous Game");
     shotBtns1.appendChild(newShot);
     shotBtns1.appendChild(saveShot);
+    shotBtns1.appendChild(editGame);
 
-    // shot number button header
-    const shotTitle = elBuilder("p", { "class": "title is-5" }, "Manage Shots")
-    shotBtns2.appendChild(shotTitle);
+    /*
+        // shot number button header
+        const shotTitle = elBuilder("p", { "class": "title is-5" }, "Manage Shots")
+        shotBtns2.appendChild(shotTitle);
 
-    // ball speed and aerial checkbox
-    const ballSpeedInput = elBuilder("input", { "id": "ballSpeedInput", "class": "input", "type": "text", "placeholder": "enter ball speed (kph)" })
-    const aerialCbx = elBuilder("input", { "id": "aerialInput", "class": "checkbox", "type": "checkbox" });
-    const aerialCbxLabel = elBuilder("label", { "class": "checkbox" }, "Aerial ", aerialCbx)
-    shotDetails.appendChild(aerialCbxLabel);
-    shotDetails.appendChild(ballSpeedInput);
+        // ball speed and aerial checkbox
+        const ballSpeedInput = elBuilder("input", { "id": "ballSpeedInput", "class": "input", "type": "text", "placeholder": "enter ball speed (kph)" })
+        const aerialCbx = elBuilder("input", { "id": "aerialInput", "class": "checkbox", "type": "checkbox" });
+        const aerialCbxLabel = elBuilder("label", { "class": "checkbox" }, "Aerial ", aerialCbx)
+        shotDetails.appendChild(aerialCbxLabel);
+        shotDetails.appendChild(ballSpeedInput);
 
-    // field images for car position
-    const fieldImageContainer = elBuilder("div", { "id": "field-img-container", "class": "container" });
-    const fieldImageBackground = elBuilder("img", { "id": "field-img-background", "src": "../images/DFH_stadium_BW90deg.png", "alt": "DFH Stadium", "style": "height: 100%; width: 100%; object-fit: contain" });
-    const fieldImageClipped = elBuilder("img", { "id": "field-img-clipped", "src": "../images/DFH_stadium_BW90deg.png", "alt": "DFH Stadium", "style": "height: 100%; width: 100%; object-fit: contain" });
-    fieldImageContainer.appendChild(fieldImageBackground);
-    fieldImageContainer.appendChild(fieldImageClipped);
-    field.appendChild(fieldImageContainer);
-
+        // field images for car position
+        const fieldImageContainer = elBuilder("div", { "id": "field-img-container", "class": "container" });
+        const fieldImageBackground = elBuilder("img", { "id": "field-img-background", "src": "../images/DFH_stadium_BW90deg.png", "alt": "DFH Stadium", "style": "height: 100%; width: 100%; object-fit: contain" });
+        const fieldImageClipped = elBuilder("img", { "id": "field-img-clipped", "src": "../images/DFH_stadium_BW90deg.png", "alt": "DFH Stadium", "style": "height: 100%; width: 100%; object-fit: contain" });
+        fieldImageContainer.appendChild(fieldImageBackground);
+        fieldImageContainer.appendChild(fieldImageClipped);
+        field.appendChild(fieldImageContainer);
+    */
   }
 
 }
