4fc6165ed0c6517f38dcbcbb1fbd54c36603b0f6
Brendan McCray <rbmccr@gmail.com>
2018-11-25 19:10:08 -0600

    finished game data layout. next step is aligning inputs better

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -8,22 +8,23 @@ const gameplay = {
     // xButton.addEventListener("click", closeBox, event); // button will display: none on parent container
     // const headerInfo = elBuilder("div", { "class": "notification is-info" }, "Create and save shots - then save the game record.", xButton);
     // webpage.appendChild(headerInfo);
-    this.buildShotContainers();
+    this.buildShotContent();
     this.buildGameContent();
-    this.addshotContent();
 
   },
 
-  buildShotContainers() {
-    // this function builds containers and adds background images
-    // important classes: level, level-left, level-item
+  buildShotContent() {
+    // this function builds shot containers and adds container content
 
     // container title
     const shotTitle = elBuilder("div", { "class": "level-item title is-4" }, "Enter Shot Data");
     const shotTitleContainer = elBuilder("div", { "class": "level" }, null, shotTitle);
 
     // new shot and save shot buttons
-    const shotButtons = elBuilder("div", { "id": "shotControls", "class": "level-item buttons" });
+    const newShot = elBuilder("button", { "id": "newShot", "class": "button is-success" }, "New Shot");
+    const saveShot = elBuilder("button", { "id": "saveShot", "class": "button is-success" }, "Save Shot");
+    const cancelShot = elBuilder("button", { "id": "cancelShot", "class": "button is-danger" }, "Cancel Shot");
+    const shotButtons = elBuilder("div", { "id": "shotControls", "class": "level-item buttons" }, null, newShot, saveShot, cancelShot);
     const alignShotButtons = elBuilder("div", { "class": "level-left" }, null, shotButtons);
     const shotButtonContainer = elBuilder("div", { "class": "level" }, null, alignShotButtons);
 
@@ -32,8 +33,8 @@ const gameplay = {
     const ballSpeedInput = elBuilder("input", { "id": "ballSpeedInput", "class": "level-item input", "placeholder": "enter ball speed" });
     const aerialOption1 = elBuilder("option", {}, "Ground shot");
     const aerialOption2 = elBuilder("option", {}, "Aerial shot");
-    const aerialSelect = elBuilder("select", { "id":"aerialInput", "class": "select" }, null, aerialOption1, aerialOption2);
-    const aerialSelectParent = elBuilder("div", {"class": "select" }, null, aerialSelect);
+    const aerialSelect = elBuilder("select", { "id": "aerialInput", "class": "select" }, null, aerialOption1, aerialOption2);
+    const aerialSelectParent = elBuilder("div", { "class": "select" }, null, aerialSelect);
     const aerialControl = elBuilder("div", { "class": "control level-item" }, null, aerialSelectParent);
     const shotDetails = elBuilder("div", { "class": "level-left" }, null, ballSpeedInputTitle, ballSpeedInput, aerialControl);
     const shotDetailsContainer = elBuilder("div", { "class": "level" }, null, shotDetails);
@@ -58,7 +59,7 @@ const gameplay = {
   },
 
   buildGameContent() {
-    // this function creates the containers that hold game content (team, )
+    // this function creates game content containers (team, game type, game mode, etc.)
 
     // container title
     const gameTitle = elBuilder("div", { "class": "level-item title is-4" }, "Enter Game Data");
@@ -76,50 +77,52 @@ const gameplay = {
     const gameTypeButtonField = elBuilder("div", { "class": "field has-addons" }, null, gameType3v3Control, gameType2v2Control, gameType1v1Control);
     const gameTypeButtonContainer = elBuilder("div", { "class": "level-item" }, null, gameTypeButtonField);
 
+    // game mode select
+    const modeOption1 = elBuilder("option", {}, "Competitive");
+    const modeOption2 = elBuilder("option", {}, "Casual");
+    const modeSelect = elBuilder("select", { "id": "gameModeInput", "class": "select" }, null, modeOption1, modeOption2);
+    const modeSelectParent = elBuilder("div", { "class": "select" }, null, modeSelect);
+    const modeControl = elBuilder("div", { "class": "control level-item" }, null, modeSelectParent);
+
     // team select
     const teamOption1 = elBuilder("option", {}, "Orange team");
     const teamOption2 = elBuilder("option", {}, "Blue Team");
-    const teamSelect = elBuilder("select", { "id":"aerialInput", "class": "select" }, null, teamOption1, teamOption2);
-    const teamSelectParent = elBuilder("div", {"class": "select" }, null, teamSelect);
+    const teamSelect = elBuilder("select", { "id": "teamInput", "class": "select" }, null, teamOption1, teamOption2);
+    const teamSelectParent = elBuilder("div", { "class": "select" }, null, teamSelect);
     const teamControl = elBuilder("div", { "class": "control level-item" }, null, teamSelectParent);
 
-    // score inputs
+    // overtime select
+    const overtimeOption1 = elBuilder("option", {}, "No overtime");
+    const overtimeOption2 = elBuilder("option", {}, "Overtime");
+    const overtimeSelect = elBuilder("select", { "id": "overtimeInput", "class": "select" }, null, overtimeOption1, overtimeOption2);
+    const overtimeSelectParent = elBuilder("div", { "class": "select" }, null, overtimeSelect);
+    const overtimeControl = elBuilder("div", { "class": "control level-item" }, null, overtimeSelectParent);
 
-
-    const gameContainerTop = elBuilder("div", { "class": "level" }, null, gameTypeButtonContainer, teamControl);
+    const gameContainerTop = elBuilder("div", { "class": "level" }, null, gameTypeButtonContainer, modeControl, teamControl, overtimeControl);
 
     // ---------- bottom container
 
-    // const
-    const gameContainerBottom = elBuilder("div", { "class": "level" });
+    // score inputs
+    // ****Note inline styling of input widths
+    const orangeScoreInputTitle = elBuilder("div", { "class": "level-item" }, "Orange team score:")
+    const orangeScoreInput = elBuilder("input", { "id": "orangeScoreInput", "class": "input", "type": "number", "placeholder": "enter orange team score" });
+    const orangeScoreControl = elBuilder("div", { "class": "level-item control" }, null, orangeScoreInput);
+    const blueScoreInputTitle = elBuilder("div", { "class": "level-item" }, "Blue team score:")
+    const blueScoreInput = elBuilder("input", { "id": "blueScoreInput", "class": "input", "type": "number", "placeholder": "enter blue team score" });
+    const blueScoreControl = elBuilder("div", { "class": "level-item control" }, null, blueScoreInput);
+    const scoreInputContainer = elBuilder("div", { "class": "level-left" }, null, orangeScoreInputTitle, orangeScoreControl, blueScoreInputTitle, blueScoreControl)
 
+    // edit/save game buttons
+    const editPreviousGame = elBuilder("button", { "id": "editPreviousGame", "class": "button is-danger" }, "Edit Previous Game");
+    const saveGame = elBuilder("button", { "id": "saveGame", "class": "button is-success" }, "Save Game");
+    const gameButtonAlignment = elBuilder("div", { "class": "buttons level-item" }, null, editPreviousGame, saveGame);
+    const gameButtonContainer = elBuilder("div", { "class": "level-right" }, null, gameButtonAlignment);
 
-    const parentGameContainer = elBuilder("div", { "class": "container box" }, null, titleContainer, gameContainerTop)
+    const gameContainerBottom = elBuilder("div", { "class": "level" }, null, scoreInputContainer, gameButtonContainer);
+
+    const parentGameContainer = elBuilder("div", { "class": "container box" }, null, titleContainer, gameContainerTop, gameContainerBottom)
 
     webpage.appendChild(parentGameContainer);
-  },
-
-  addshotContent() {
-    // this function creates and appends the user interactivity features to the containers (buttons, inputs, etc.)
-
-    // new shot and save shot buttons
-    const newShot = elBuilder("button", { "id": "newShot", "class": "button is-success" }, "New Shot");
-    const saveShot = elBuilder("button", { "id": "saveShot", "class": "button is-success" }, "Save Shot");
-    const cancelShot = elBuilder("button", { "id": "cancelShot", "class": "button is-danger" }, "Cancel Shot");
-    const shot1 = elBuilder("button", { "id": "editGame", "class": "button is-link", "title": "Click to edit" }, "Shot 1");
-    const shot2 = elBuilder("button", { "id": "editGame", "class": "button is-link" }, "Shot 2");
-
-    // DOM container references
-    const shotBtns = document.getElementById("shotControls");
-
-    shotBtns.appendChild(newShot);
-    shotBtns.appendChild(saveShot);
-    shotBtns.appendChild(cancelShot);
-    shotBtns.appendChild(shot1);
-    shotBtns.appendChild(shot2);
-
-    // const editGame = elBuilder("button", { "id": "editGame", "class": "button is-danger" }, "Edit Previous Game");
-
   }
 
 }
